4f4001c357e361c8e422e039d1ad986b4e639fd8
Luna Dagci <ldagci@albany.edu>
2022-02-25 20:38:48 -0500

    Using Glide to pull the image using the URLs from API

--- app/src/main/java/com/luna/flixster/MovieAdapter.kt
+++ app/src/main/java/com/luna/flixster/MovieAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MovieAdapter(private val context : Context, private val movies: List<Movie>) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -31,7 +32,7 @@ class MovieAdapter(private val context : Context, private val movies: List<Movie
         fun bind(movie: Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
-            //todo populate imageview
+            Glide.with(context).load(movie.posterImageURL).into(ivPoster)
         }
     }
 }
