99f950f92a527b0ee07d28379e4b1ff26885f81d
Luna Dagci <ldagci@albany.edu>
2022-02-25 19:15:21 -0500

    Parsing JSON

--- app/src/main/java/com/luna/flixster/MainActivity.kt
+++ app/src/main/java/com/luna/flixster/MainActivity.kt
@@ -6,10 +6,12 @@ import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private const val NOW_PLAYING_URL= "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private val movies = mutableListOf<Movie>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +24,14 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG, "onSuccess: JSON data $json")
-                val movieJSONarray = json.jsonObject.getJSONArray("results")
+                try {
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    Log.i(TAG, "Movie list $movies")
+                }
+                catch (e: JSONException){
+                    Log.e(TAG, "Encountered error $e")
+                }
             }
 
         })
